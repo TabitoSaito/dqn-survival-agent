093769158e8268223ccb0b2edc03cf933647a71f
TabitoSaito <domi2601.hoelzl@gmail.com>
2025-12-10 21:04:29 +0100

    Change hyperparameter-tuning to use class based train-loop

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ from agents.dqn_agent import DQNAgent, DoubleDQNAgent, DoubleDQNAgentPER
 from train.train_loop import TrainLoop, prebuilt_train_loop
 from train.evaluation import render_run, eval_agent
 
-# from train.hyperparameter_tuning import optimize_agent
+from train.hyperparameter_tuning import optimize_agent
 
 with open("configs/envs/default.yaml") as stream:
     env_config = yaml.safe_load(stream)
@@ -25,15 +25,15 @@ state, info = env.reset()
 num_actions = env.action_space.n
 num_obs = len(state)
 
-agent = DoubleDQNAgent(num_actions, num_obs, config=agent_config, network=DuelingDQN)
+# agent = DoubleDQNAgent(num_actions, num_obs, config=agent_config, network=DuelingDQN)
 
-prebuilt_train_loop(agent, env)
+# prebuilt_train_loop(agent, env)
 
-render_run(agent, env, "test", runs=10)
+# render_run(agent, env, "test", runs=10)
 
 # eval_agent(agent, env)
 
-# with open("configs/hyperparameter_tuning/default.yaml") as stream:
-#     agent_config = yaml.safe_load(stream)
+with open("configs/hyperparameter_tuning/default.yaml") as stream:
+    agent_config = yaml.safe_load(stream)
 
-# optimize_agent(100, agent_config, DuelingDQN, DoubleDQNAgent, env, episodes=50)
+optimize_agent(100, agent_config, DuelingDQN, DoubleDQNAgent, env, max_episodes=2000)
--- src/train/hyperparameter_tuning.py
+++ src/train/hyperparameter_tuning.py
@@ -1,4 +1,4 @@
-from train.train_loop import train_loop
+from train.train_loop import TrainLoop
 import optuna
 from functools import partial
 from typing import Optional, Iterable
@@ -6,8 +6,8 @@ import numpy as np
 import copy
 
 
-def objective(trial, config, network, agent, env, episodes: int = 200, loops: int = 10, seeds: Optional[Iterable[int]] = None):
-    assert episodes > 0, "episodes argument has to be bigger than 0"
+def objective(trial, config, network, agent, env, max_episodes: int = 2000, loops: int = 10, seeds: Optional[Iterable[int]] = None):
+    assert max_episodes > 0, "episodes argument has to be bigger than 0"
 
     if seeds is None:
         seeds = [i for i in range(10)]
@@ -26,6 +26,7 @@ def objective(trial, config, network, agent, env, episodes: int = 200, loops: in
 
     scores = []
     aucs = []
+    train_len = []
     for loop_idx in range(loops):
         cur_env = copy.deepcopy(env)
 
@@ -36,18 +37,32 @@ def objective(trial, config, network, agent, env, episodes: int = 200, loops: in
 
         cur_agent = agent(num_actions, num_obs, config=converted_config, network=network)
 
-        score, auc = train_loop(cur_agent, cur_env, episodes=episodes, seeds=seeds, dyn_print=False, plot=False)
+        loop = TrainLoop(cur_agent, cur_env, seeds=seeds, dyn_print=False, plot=False)
+        while loop.cur_episode < max_episodes:
+            loop.episode_step()
+            if loop.cur_episode - loop.best_episode > 200:
+                break
+        
+        trial.set_user_attr("Episodes", loop.cur_episode)
+
+        score = np.mean(loop.scores[-100:])
+        auc = np.trapezoid(loop.scores) / loop.cur_episode
+
         scores.append(score)
         aucs.append(auc)
+        train_len.append(loop.cur_episode)
 
     scores.sort()
     aucs.sort()
+
+    trial.set_user_attr("Episodes", np.mean(train_len))
+
     return np.median(scores), np.median(aucs)
 
-def optimize_agent(n_trials: int, config, network, agent, env, episodes: int = 200, loops: int = 5, seeds: Optional[Iterable[int]] = None):
+def optimize_agent(n_trials: int, config, network, agent, env, max_episodes: int = 2000, loops: int = 5, seeds: Optional[Iterable[int]] = None):
     study = optuna.create_study(directions=["maximize", "maximize"], storage="sqlite:///instance/db.sqlite3")
 
-    par_objective = partial(objective, config=config, network=network, agent=agent, env=env, episodes=episodes, loops=loops, seeds=seeds)
+    par_objective = partial(objective, config=config, network=network, agent=agent, env=env, max_episodes=max_episodes, loops=loops, seeds=seeds)
 
     study.optimize(par_objective, n_trials=n_trials, show_progress_bar=True)
     
